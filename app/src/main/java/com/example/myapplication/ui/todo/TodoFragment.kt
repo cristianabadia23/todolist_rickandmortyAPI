b55ca100b3e65df0985eadcdeb8120ebef39609c
DESKTOP-CRB5D67\lisbeth <cristianrabadia@gmail.com>
2025-03-28 06:06:48 +0100

    refactor

--- app/src/main/java/com/example/myapplication/ui/todo/TodoFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/todo/TodoFragment.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication.ui.home
+package com.example.myapplication.ui.todo
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.myapplication.databinding.FragmentHomeBinding
+import com.example.myapplication.databinding.FragmentTodoBinding
 
-class HomeFragment : Fragment() {
+class TodoFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentTodoBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,15 +23,11 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this).get(TodoViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentTodoBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
--- app/src/main/java/com/example/myapplication/ui/todo/TodoViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/todo/TodoViewModel.kt
@@ -1,13 +1,11 @@
-package com.example.myapplication.ui.home
+package com.example.myapplication.ui.todo
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class TodoViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
     }
-    val text: LiveData<String> = _text
 }
